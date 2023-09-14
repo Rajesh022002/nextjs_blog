a667d5fd014faadfb36e31b3a8730bd846232773
Rajesh <rajesh11022002@gmail.com>
2023-09-14 21:28:16 +0530

    added exportPathmap in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+    exportPathMap: async function (defaultPathMap) {
+        delete defaultPathMap['/posts/[id]']; // Exclude the problematic page
+        return defaultPathMap;
+    },
     images: {
         domains: ['task.appsdeployer.com'],
     },
